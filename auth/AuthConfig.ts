bb8edd57a08da712c16fccde451918e19727b948
rakern <rakern@crimson.ua.edu>
2023-03-21 11:37:12 -0500

    adding all

---
+++ auth/AuthConfig.ts
@@ -0,0 +1,11 @@
+export const AuthConfig = {
+  appId: '46be6313-02fe-493c-ad58-68df79ac48de',
+  appScopes: [
+    'openid',
+    'offline_access',
+    'profile',
+    'User.Read',
+    'MailboxSettings.Read',
+    'Calendars.ReadWrite',
+  ],
+};
